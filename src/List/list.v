ba614fdaefd51ad299288be06f64c1093ee724e7
David Cohoe <sudgylacmoe@gmail.com>
2021-12-16 23:20:57 -0800

    Split list into list_func

--- src/List/list.v
+++ src/List/list.v
@@ -1,38 +1,11 @@
 Require Import init.
 
 Require Export list_base.
+Require Export list_func.
 
 Require Import nat.
 
 Set Implicit Arguments.
-
-Fixpoint list_image (A B : Type) (l : list A) (f : A → B) :=
-    match l with
-    | list_end => list_end
-    | a :: l' => f a :: list_image l' f
-    end.
-
-Theorem list_image_conc {A B : Type} : ∀ (l1 l2 : list A) (f : A → B),
-        list_image (l1 ++ l2) f = list_image l1 f ++ list_image l2 f.
-    intros l1 l2 f.
-    induction l1.
-    -   cbn.
-        reflexivity.
-    -   cbn.
-        rewrite IHl1.
-        reflexivity.
-Qed.
-
-Theorem list_image_comp {A B C : Type} : ∀ (l : list A) (f : A → B) (g : B → C),
-        list_image (list_image l f) g = list_image l (λ x, g (f x)).
-    intros l f g.
-    induction l.
-    -   cbn.
-        reflexivity.
-    -   cbn.
-        rewrite IHl.
-        reflexivity.
-Qed.
 
 Fixpoint in_list (A : Type) (l : list A) (x : A) :=
     match l with
@@ -132,50 +105,6 @@ Theorem image_in_list {U V} : ∀ l y (f : U → V),
             *   right.
                 exact x_in.
 Qed.
-
-Fixpoint list_prod2_base {A B : Type} (op : A → A → B) (l : list A) (b : A) :=
-    match l with
-    | list_end => list_end
-    | a :: l' => op a b :: list_prod2_base op l' b
-    end.
-
-Fixpoint list_prod2 {A B : Type} (op : A → A → B) (l1 l2 : list A) :=
-    match l2 with
-    | list_end => list_end
-    | b :: l2' => list_prod2_base op l1 b ++ list_prod2 op l1 l2'
-    end.
-
-Theorem list_prod2_lend {A B : Type} (op : A → A → B) (l : list A) :
-        list_prod2 op list_end l = list_end.
-    induction l.
-    -   cbn.
-        reflexivity.
-    -   cbn.
-        exact IHl.
-Qed.
-
-Theorem list_prod2_rend {A B : Type} (op : A → A → B) (l : list A) :
-        list_prod2 op l list_end = list_end.
-    cbn.
-    reflexivity.
-Qed.
-
-Theorem list_prod2_base_image {A B} (op : A → A → B) : ∀ l b,
-        list_prod2_base op l b = list_image l (λ x, op x b).
-    intros l b.
-    induction l.
-    -   cbn.
-        reflexivity.
-    -   cbn.
-        rewrite IHl.
-        reflexivity.
-Qed.
-
-Fixpoint list_zip {A B : Type} (l1 : list A) (l2 : list B) :=
-    match l1, l2 with
-    | a :: l1', b :: l2' => (a, b) :: list_zip l1' l2'
-    | _, _ => list_end
-    end.
 
 Fixpoint list_size {A : Type} (l : list A) :=
     match l with
