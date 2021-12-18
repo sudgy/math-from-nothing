6140d1fe641e7c05afd3f6872f86f26acad781b0
David Cohoe <sudgylacmoe@gmail.com>
2021-12-17 16:04:34 -0800

    Added unordered_list_base

---
+++ src/List/unordered_list_base.v
@@ -0,0 +1,104 @@
+Require Import init.
+
+Require Import list_perm.
+
+Require Import equivalence.
+
+Instance list_perm_reflexive U : Reflexive _ := {
+    refl := @list_perm_refl U
+}.
+Instance list_perm_symmetric U : Symmetric _ := {
+    sym := @list_perm_sym U
+}.
+Instance list_perm_transitive U : Transitive _ := {
+    trans := @list_perm_trans U
+}.
+
+Definition ulist_equiv U := make_equiv _
+    (list_perm_reflexive U) (list_perm_symmetric U) (list_perm_transitive U).
+Notation "'ulist' U" := (equiv_type (ulist_equiv U)) (at level 1).
+
+Definition ulist_end {U} := to_equiv_type (ulist_equiv U) list_end.
+
+Lemma uadd_wd U : ∀ l1 l2 (a : U),
+        list_permutation l1 l2 → list_permutation (a :: l1) (a :: l2).
+    intros l1 l2 a l_perm.
+    apply list_perm_skip.
+    exact l_perm.
+Qed.
+Definition ulist_add {U} := binary_rself_op (E := ulist_equiv U) (uadd_wd U).
+Infix ":::" := ulist_add (at level 60, right associativity) : list_scope.
+
+Theorem ulist_induction {U} : ∀ S : ulist U → Prop,
+        S ulist_end → (∀ a l, S l → S (a ::: l)) → ∀ l, S l.
+    intros S S_end S_add l.
+    equiv_get_value l.
+    induction l.
+    -   exact S_end.
+    -   assert (to_equiv_type (ulist_equiv U) (a :: l) =
+            a ::: (to_equiv_type (ulist_equiv U) l)) as eq.
+        {
+            unfold ulist_add; equiv_simpl.
+            apply list_perm_refl.
+        }
+        rewrite eq.
+        apply S_add.
+        exact IHl.
+Qed.
+
+Lemma uconc_wd U : ∀ al1 al2 bl1 bl2 : list U,
+        list_permutation al1 al2 → list_permutation bl1 bl2 →
+        list_permutation (al1 ++ bl1) (al2 ++ bl2).
+    intros al1 al2 bl1 bl2 eq1 eq2.
+    pose proof (list_perm_rpart al1 eq2).
+    pose proof (list_perm_lpart bl2 eq1).
+    exact (list_perm_trans H H0).
+Qed.
+Definition ulist_conc {U} := binary_self_op (E := ulist_equiv U) (uconc_wd U).
+Infix "+++" := ulist_conc (right associativity, at level 60) : list_scope.
+
+Theorem ulist_add_conc_add {U} : ∀ (a : U) l1 l2,
+        a ::: (l1 +++ l2) = (a ::: l1) +++ l2.
+    intros a l1 l2.
+    equiv_get_value l1 l2.
+    unfold ulist_conc, ulist_add; equiv_simpl.
+    apply list_perm_refl.
+Qed.
+
+Theorem ulist_add_conc {U} : ∀ (a : U) l, a ::: l = (a ::: ulist_end) +++ l.
+    intros a l.
+    equiv_get_value l.
+    unfold ulist_end, ulist_add, ulist_conc; equiv_simpl.
+    apply list_perm_refl.
+Qed.
+
+Theorem ulist_conc_lid {U} : ∀ l : ulist U, ulist_end +++ l = l.
+    intros l.
+    equiv_get_value l.
+    unfold ulist_end, ulist_conc; equiv_simpl.
+    apply list_perm_refl.
+Qed.
+
+Theorem ulist_conc_rid {U} : ∀ l : ulist U, l +++ ulist_end = l.
+    intros l.
+    equiv_get_value l.
+    unfold ulist_end, ulist_conc; equiv_simpl.
+    rewrite list_conc_end.
+    apply list_perm_refl.
+Qed.
+
+Theorem ulist_conc_comm {U} : ∀ a b : ulist U, a +++ b = b +++ a.
+    intros a b.
+    equiv_get_value a b.
+    unfold ulist_conc; equiv_simpl.
+    apply list_perm_conc.
+Qed.
+
+Theorem ulist_conc_assoc {U} : ∀ a b c : ulist U,
+        a +++ (b +++ c) = (a +++ b) +++ c.
+    intros a b c.
+    equiv_get_value a b c.
+    unfold ulist_conc; equiv_simpl.
+    rewrite list_conc_assoc.
+    apply list_perm_refl.
+Qed.
