52674e96c78c75132c9d8ed7f7f255f94b5b77e3
David Cohoe <sudgylacmoe@gmail.com>
2022-01-07 01:05:53 -0800

    Added more definitions in mult_div

--- src/Algebra/Ring/Domain/mult_div.v
+++ src/Algebra/Ring/Domain/mult_div.v
@@ -1,6 +1,6 @@
 Require Import init.
 
-Require Import mult_ring.
+Require Export mult_ring.
 Require Import relation.
 
 Definition divides {U} `{Mult U} a b := ∃ c, c * a = b.
@@ -9,6 +9,13 @@ The reason for this is that using the normal '|' causes issues with things like
 pattern matching.
 *)
 Infix "∣" := divides (at level 50).
+
+Definition unit {U} `{Mult U, One U} a := a ∣ 1.
+Definition associates {U} `{Mult U} a b := a ∣ b ∧ b ∣ a.
+Definition irreducible {U} `{Zero U, Mult U, One U} p
+    := 0 ≠ p ∧ ¬unit p ∧ ∀ a b, ¬unit a → ¬unit b → p ≠ a * b.
+Definition prime {U} `{Zero U, Mult U, One U} p
+    := 0 ≠ p ∧ ¬unit p ∧ ∀ a b, p ∣ (a * b) → p ∣ a ∨ p ∣ b.
 
 Definition even {U} `{Plus U, Mult U, One U} a := 2 ∣ a.
 Definition odd {U} `{Plus U, Mult U, One U} a := ¬(2 ∣ a).
@@ -122,6 +129,72 @@ Theorem mult_factors_back : ∀ a b c, a * b = c → a ∣ c ∧ b ∣ c.
         exact eq.
     -   exists a.
         exact eq.
+Qed.
+
+Theorem mult_div_lself : ∀ a b, a ∣ a * b.
+    intros a b.
+    exists b.
+    apply mult_comm.
+Qed.
+
+Theorem mult_div_rself : ∀ a b, a ∣ b * a.
+    intros a b.
+    exists b.
+    reflexivity.
+Qed.
+
+Theorem div_rcancel : ∀ a b c, 0 ≠ c → a * c ∣ b * c → a ∣ b.
+    intros a b c c_nz [x eq].
+    exists x.
+    rewrite mult_assoc in eq.
+    apply mult_rcancel in eq; [>|exact c_nz].
+    exact eq.
+Qed.
+
+Theorem div_lcancel : ∀ a b c, 0 ≠ a → a * b ∣ a * c → b ∣ c.
+    intros a b c a_nz.
+    do 2 rewrite (mult_comm a).
+    apply div_rcancel.
+    exact a_nz.
+Qed.
+
+Theorem div_mult_unit : ∀ a b, 0 ≠ a → a * b ∣ a → unit b.
+    intros a b a_nz eq.
+    destruct eq as [c eq].
+    exists c.
+    rewrite (mult_comm a b) in eq.
+    rewrite mult_assoc in eq.
+    rewrite <- (mult_lid a) in eq at 2.
+    apply mult_rcancel in eq; [>|exact a_nz].
+    exact eq.
+Qed.
+
+Theorem prime_irreducible : ∀ p, prime p → irreducible p.
+    intros p [p_nz [p_nu p_prime]].
+    repeat split; [>exact p_nz|exact p_nu|].
+    intros a b a_nu b_nu.
+    intros contr.
+    subst p.
+    assert (0 ≠ a) as a_nz.
+    {
+        intros contr.
+        subst a.
+        rewrite mult_lanni in p_nz.
+        contradiction.
+    }
+    assert (0 ≠ b) as b_nz.
+    {
+        intros contr.
+        subst b.
+        rewrite mult_ranni in p_nz.
+        contradiction.
+    }
+    specialize (p_prime a b (refl (a * b))) as [d1|d2].
+    -   apply div_mult_unit in d1; [>|exact a_nz].
+        contradiction.
+    -   rewrite mult_comm in d2.
+        apply div_mult_unit in d2; [>|exact b_nz].
+        contradiction.
 Qed.
 
 (* begin hide *)
