f84e78f281b3d392331e641270e328ab6885a987
David Cohoe <sudgylacmoe@gmail.com>
2022-01-16 13:59:54 -0800

    Defined the reverse in GA

--- src/Algebra/Geometric/geometric_involutions.v
+++ src/Algebra/Geometric/geometric_involutions.v
@@ -140,4 +140,145 @@ Theorem ga_conjugate_vector : ∀ v, (φ v)∗ = -φ v.
     apply [|ga_conjugate_base].
 Qed.
 
+
+Definition ga_op := ga B.
+Local Instance ga_op_mult : Mult ga_op := {
+    mult a b := b * a
+}.
+Local Program Instance ga_op_ldist : Ldist ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    apply rdist.
+Qed.
+Local Program Instance ga_op_rdist : Rdist ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    apply ldist.
+Qed.
+Local Program Instance ga_op_mult_assoc : MultAssoc ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    symmetry; apply mult_assoc.
+Qed.
+Local Instance ga_op_one : One ga_op := {
+    one := one
+}.
+Local Program Instance ga_op_mult_lid : MultLid ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    apply mult_rid.
+Qed.
+Local Program Instance ga_op_mult_rid : MultRid ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    apply mult_lid.
+Qed.
+Local Program Instance ga_op_scalar_lmult : ScalarLMult (cring_U F) ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    apply scalar_rmult.
+Qed.
+Local Program Instance ga_op_scalar_rmult : ScalarRMult (cring_U F) ga_op.
+Next Obligation.
+    unfold mult; cbn.
+    apply scalar_lmult.
+Qed.
+
+Definition ga_op_algebra := make_algebra
+    F
+    (algebra_module (geometric_algebra B))
+    ga_op_mult
+    ga_op_ldist
+    ga_op_rdist
+    ga_op_mult_assoc
+    ga_op_one
+    ga_op_mult_lid
+    ga_op_mult_rid
+    ga_op_scalar_lmult
+    ga_op_scalar_rmult
+.
+
+Definition ga_reverse_base1 (v : module_V V) := φ v : ga_op.
+
+Lemma ga_reverse_base_plus : ∀ u v, ga_reverse_base1 (u + v) =
+        ga_reverse_base1 u + ga_reverse_base1 v.
+    intros u v.
+    unfold ga_reverse_base1.
+    apply vector_to_ga_plus.
+Qed.
+
+Lemma ga_reverse_base_scalar : ∀ a v,
+        ga_reverse_base1 (a · v) = a · ga_reverse_base1 v.
+    intros a v.
+    unfold ga_reverse_base1.
+    apply vector_to_ga_scalar.
+Qed.
+
+Definition ga_reverse_base2 := make_module_homomorphism
+    F
+    V
+    (algebra_module ga_op_algebra)
+    ga_reverse_base1
+    ga_reverse_base_plus
+    ga_reverse_base_scalar.
+
+Lemma ga_reverse_base_contract : ∀ v,
+        ga_reverse_base1 v * ga_reverse_base1 v = [B|] v v · 1.
+    intros v.
+    unfold ga_reverse_base1.
+    unfold mult; cbn.
+    apply ga_contract.
+Qed.
+
+Definition ga_reverse_base3 := make_to_ga
+    B
+    ga_op_algebra
+    ga_reverse_base2
+    ga_reverse_base_contract.
+
+Definition ga_reverse_base
+    := card_one_ex (geometric_universal B ga_reverse_base3).
+
+Definition ga_reverse_homo := [ga_reverse_base|].
+Definition ga_reverse := algebra_homo_f ga_reverse_homo : ga B → ga B.
+
+Local Notation "a '†'" := (ga_reverse a) (at level 10).
+
+Theorem ga_reverse_plus : ∀ u v, (u + v)† = u† + v†.
+    apply (algebra_homo_plus _ _ ga_reverse_homo).
+Qed.
+
+Theorem ga_reverse_scalar : ∀ a v, (a · v)† = a · v†.
+    apply (algebra_homo_scalar _ _ ga_reverse_homo).
+Qed.
+
+Theorem ga_reverse_mult : ∀ u v,
+        (@mult _ (ga_mult B) u v)† = @mult _ (ga_mult B) (v†) (u†).
+    apply (algebra_homo_mult _ _ ga_reverse_homo).
+Qed.
+
+Theorem ga_reverse_one : (@one _ (ga_one B))† = (@one _ (ga_one B)) .
+    apply (algebra_homo_one _ _ ga_reverse_homo).
+Qed.
+
+Theorem ga_reverse_neg : ∀ v, (-v)† = -v†.
+    apply (algebra_homo_neg ga_reverse_homo).
+Qed.
+
+Theorem ga_reverse_zero : 0† = 0.
+    apply (algebra_homo_zero ga_reverse_homo).
+Qed.
+
+Theorem ga_reverse_of_scalar : ∀ a, (σ a)† = σ a.
+    intros a.
+    rewrite scalar_to_ga_one_scalar.
+    rewrite ga_reverse_scalar.
+    rewrite ga_reverse_one.
+    reflexivity.
+Qed.
+
+Theorem ga_reverse_vector : ∀ v, (φ v)† = φ v.
+    apply [|ga_reverse_base].
+Qed.
+
 End GeometricInvolutions.
