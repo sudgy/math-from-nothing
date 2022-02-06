8dfd8e7fdc546cb56630db21ea13396214f31bed
David Cohoe <sudgylacmoe@gmail.com>
2022-02-06 13:58:51 -0800

    Added applys_eq from TLC

--- src/Init/init.v
+++ src/Init/init.v
@@ -1,6 +1,7 @@
 Require Export Coq.Classes.Init.
 Require Export logic.
 Require Export tactics.
+Require Export tlc_tactics.
 Require Export notations.
 Require Export types.
 Global Set Keyed Unification.
