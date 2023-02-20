8f6b15b111ce4bf9a445a2920dcd322081e8cc90
David Cohoe <sudgylacmoe@gmail.com>
2023-02-20 11:44:18 -0800

    Generalized two lemmas in wo theorem proof

--- src/Set/well_order.v
+++ src/Set/well_order.v
@@ -4,19 +4,6 @@ Require Import zorn.
 Require Import zorn_set.
 Require Import set.
 
-Theorem strict_subset_ex {U} : ∀ (S T : U → Prop), S ⊂ T → ∃ x, ¬S x ∧ T x.
-Proof.
-    intros S T sub.
-    classic_contradiction contr.
-    apply sub.
-    apply antisym; [>apply sub|].
-    intros x Tx.
-    rewrite not_ex in contr.
-    specialize (contr x).
-    rewrite and_comm, not_and_impl, not_not in contr.
-    exact (contr Tx).
-Qed.
-
 Section WellOrderingTheorem.
 
 Context {U : Type}.
@@ -141,63 +128,109 @@ Proof.
     exact ltq.
 Qed.
 
+Lemma wo_conforming_add : ∀ S T, wo_conforming S →
+    (∃ X, T - ❴X❵ = S ∧ ⋃S ⊆ X ∧ ∀ x, X x → ¬(⋃S) x → S (X - ❴x❵)) →
+    wo_conforming T.
+Proof.
+    intros SS TT SS_conf [X [X_eq [X_sub X_in]]].
+    assert (SS ⊆ TT) as sub.
+    {
+        intros x Sx.
+        rewrite <- X_eq in Sx.
+        apply Sx.
+    }
+    split.
+    -   intros x [A [TT_A Ax]].
+        classic_case ((⋃ SS) x) as [x_ex|x_nex].
+        +   destruct x_ex as [B [SS_B Bx]].
+            pose proof (land SS_conf x (ex_intro _ _ (make_and SS_B Bx)))
+                as [S [Sx [SS_S SS_Sx]]].
+            exists S.
+            apply sub in SS_S, SS_Sx.
+            repeat split; assumption.
+        +   assert (A = X) as eq.
+            {
+                rewrite not_ex in x_nex.
+                specialize (x_nex A).
+                rewrite and_comm, not_and_impl in x_nex.
+                apply x_nex in Ax.
+                rewrite <- X_eq in Ax.
+                unfold set_minus in Ax.
+                rewrite not_and_impl, not_not in Ax.
+                symmetry; apply Ax.
+                exact TT_A.
+            }
+            subst A.
+            exists X.
+            split; [>exact Ax|].
+            split; [>exact TT_A|].
+            apply sub.
+            apply X_in; assumption.
+    -   intros AA AA_sub [A AA_A].
+        classic_case (∃ Y, AA Y ∧ SS Y) as [Y_ex|Y_nex].
+        +   destruct Y_ex as [Y [AA_Y SS_Y]].
+            pose proof (rand SS_conf (AA ∩ SS)
+                (inter_rsub _ _) (ex_intro _ _ (make_and AA_Y SS_Y)))
+                as [a [[AA_a SS_a] a_least]].
+            exists a.
+            split; [>exact AA_a|].
+            intros y AA_y.
+            classic_case (SS y) as [SS_y|SS_y].
+            *   apply a_least.
+                split; assumption.
+            *   apply AA_sub in AA_y.
+                rewrite <- X_eq in SS_y.
+                unfold set_minus in SS_y.
+                rewrite not_and_impl, not_not in SS_y.
+                apply SS_y in AA_y.
+                rewrite singleton_eq in AA_y.
+                subst y.
+                intros s sa.
+                apply X_sub.
+                exists a.
+                split; assumption.
+        +   exists X.
+            assert (∀ Z, AA Z → Z = X) as Z_eq.
+            {
+                intros Z SZ.
+                rewrite not_ex in Y_nex.
+                specialize (Y_nex Z).
+                rewrite not_and_impl in Y_nex.
+                specialize (Y_nex SZ).
+                rewrite <- X_eq in Y_nex.
+                unfold set_minus in Y_nex.
+                rewrite not_and_impl, not_not in Y_nex.
+                apply AA_sub in SZ.
+                apply Y_nex in SZ.
+                symmetry; exact SZ.
+            }
+            rewrite (Z_eq _ AA_A) in AA_A.
+            split; [>exact AA_A|].
+            intros y AA_y.
+            rewrite (Z_eq _ AA_y).
+            apply refl.
+Qed.
+
 Lemma well_order_sets_union : well_order_sets (⋃ well_order_sets).
 Proof.
     classic_contradiction contr.
     pose (A := well_order_sets ∪ ❴⋃ well_order_sets❵).
     assert (wo_conforming A) as A_wo.
     {
-        unfold A.
-        split.
-        -   intros x [B [AB Bx]].
-            destruct AB as [AB|B_eq].
-            +   pose proof (land well_order_sets_wo x
-                    (ex_intro _ _ (make_and AB Bx))) as [S [Sx [S_wo S_wo']]].
-                exists S.
-                split; [>exact Sx|].
-                split; left; assumption.
-            +   rewrite singleton_eq in B_eq; subst B.
-                destruct Bx as [C [C_wo Cx]].
-                pose proof (land well_order_sets_wo x
-                    (ex_intro _ _ (make_and C_wo Cx))) as [S [Sx [S_wo S_wo']]].
-                exists S.
-                split; [>exact Sx|].
-                split; left; assumption.
-        -   intros S S_sub [X SX].
-            classic_case (∃ Y, S Y ∧ well_order_sets Y) as [Y_ex|Y_nex].
-            +   destruct Y_ex as [Y [SY Y_wo]].
-                pose proof (rand well_order_sets_wo (S ∩ well_order_sets)
-                    (inter_rsub _ _) (ex_intro _ _ (make_and SY Y_wo)))
-                    as [a [[Sa a_wo] a_least]].
-                exists a.
-                split; [>exact Sa|].
-                intros y Sy.
-                classic_case (well_order_sets y) as [y_wo|y_nwo].
-                *   apply a_least.
-                    split; assumption.
-                *   apply S_sub in Sy.
-                    destruct Sy as [y_wo|w_eq]; [>contradiction|].
-                    rewrite singleton_eq in w_eq; subst y.
-                    intros x ax.
-                    exists a.
-                    split; assumption.
-            +   exists X.
-                split; [>exact SX|].
-                intros y Sy.
-                assert (∀ Z, S Z → Z = ⋃ well_order_sets) as Z_eq.
-                {
-                    intros Z SZ.
-                    rewrite not_ex in Y_nex.
-                    specialize (Y_nex Z).
-                    rewrite not_and_impl in Y_nex.
-                    specialize (Y_nex SZ).
-                    apply S_sub in SZ.
-                    destruct SZ as [SZ|SZ]; [>contradiction|].
-                    symmetry; exact SZ.
-                }
-                rewrite (Z_eq _ SX).
-                rewrite (Z_eq _ Sy).
-                apply refl.
+        apply (wo_conforming_add well_order_sets).
+        -   apply well_order_sets_wo.
+        -   exists (⋃ well_order_sets).
+            unfold A.
+            split; [>|split].
+            +   apply union_minus.
+                apply empty_eq.
+                intros x [x_in1 x_in2].
+                apply contr.
+                rewrite singleton_eq in x_in2; subst x.
+                exact x_in1.
+            +   apply refl.
+            +   intros x x_in x_nin.
+                contradiction.
     }
     apply (well_order_sets_max [A|A_wo]).
     split.
@@ -228,85 +261,28 @@ Proof.
     pose (A := well_order_sets ∪ ❴⋃ well_order_sets ∪ ❴x❵❵).
     assert (wo_conforming A) as A_wo.
     {
-        split.
-        -   intros a [B [AB Ba]].
-            assert (∀ X, well_order_sets X → X a → ∃ S, S a ∧ A S ∧ A (S - ❴a❵))
-                as wlog.
-            {
-                intros X X_wo Xa.
-                pose proof (land well_order_sets_wo a
-                    (ex_intro _ _ (make_and X_wo Xa))) as [Y [Ya [Y_in Y_in']]].
-                exists Y.
-                split; [>exact Ya|].
-                split; left; assumption.
-            }
-            destruct AB as [B_wo|B_eq].
-            +   apply (wlog B B_wo Ba).
-            +   rewrite singleton_eq in B_eq; subst B.
-                destruct Ba as [a_in|a_eq].
-                *   destruct a_in as [X [X_wo Xa]].
-                    apply (wlog X X_wo Xa).
-                *   rewrite singleton_eq in a_eq; subst a.
-                    exists (⋃ well_order_sets ∪ ❴x❵).
-                    split; [>|split].
-                    --  right.
-                        reflexivity.
-                    --  right.
-                        reflexivity.
-                    --  left.
-                        assert (⋃ well_order_sets ∪ ❴x❵ - ❴x❵ = ⋃ well_order_sets) as eq.
-                        {
-                            apply antisym.
-                            -   intros y [y_in y_in'].
-                                destruct y_in as [y_in|]; [>|contradiction].
-                                exact y_in.
-                            -   intros y y_in.
-                                split; [>left; exact y_in|].
-                                intros contr'.
-                                rewrite singleton_eq in contr'; subst y.
-                                apply contr.
-                                destruct y_in as [B [B_wo Bx]].
-                                exists B.
-                                split; assumption.
-                        }
-                        rewrite eq.
-                        apply well_order_sets_union.
-        -   intros S S_sub [X SX].
-            classic_case (∃ Y, S Y ∧ well_order_sets Y) as [Y_ex|Y_nex].
-            +   destruct Y_ex as [Y [SY Y_wo]].
-                pose proof (rand well_order_sets_wo (S ∩ well_order_sets)
-                    (inter_rsub _ _) (ex_intro _ _ (make_and SY Y_wo)))
-                    as [a [[Sa a_wo] a_least]].
-                exists a.
-                split; [>exact Sa|].
-                intros y Sy.
-                classic_case (well_order_sets y) as [y_wo|y_nwo].
-                *   apply a_least.
-                    split; assumption.
-                *   apply S_sub in Sy.
-                    destruct Sy as [y_wo|w_eq]; [>contradiction|].
-                    rewrite singleton_eq in w_eq; subst y.
-                    intros y ay.
-                    left.
-                    exists a.
-                    split; assumption.
-            +   exists X.
-                split; [>exact SX|].
-                intros y Sy.
-                assert (∀ Z, S Z → Z = ⋃ well_order_sets ∪ ❴x❵) as Z_eq.
-                {
-                    intros Z SZ.
-                    rewrite not_ex in Y_nex.
-                    specialize (Y_nex Z).
-                    rewrite not_and_impl in Y_nex.
-                    specialize (Y_nex SZ).
-                    apply S_sub in SZ.
-                    destruct SZ as [SZ|SZ]; [>contradiction|].
-                    symmetry; exact SZ.
-                }
-                rewrite (Z_eq _ SX).
-                rewrite (Z_eq _ Sy).
-                apply refl.
+        apply (wo_conforming_add well_order_sets).
+        -   exact well_order_sets_wo.
+        -   exists (⋃ well_order_sets ∪ ❴x❵).
+            unfold A.
+            split; [>|split].
+            +   apply union_minus.
+                apply empty_eq.
+                intros a [a_in1 a_in2].
+                rewrite singleton_eq in a_in2; subst a.
+                apply contr.
+                exists (⋃ well_order_sets ∪ ❴x❵).
+                split; [>|exact a_in1].
+                right; reflexivity.
+            +   apply union_lsub.
+            +   intros a a_in a_nin.
+                destruct a_in as [a_in|a_eq]; [>contradiction|].
+                rewrite singleton_eq in a_eq; subst a.
+                rewrite union_minus; [>apply well_order_sets_union|].
+                apply empty_eq.
+                intros a [a_in1 a_in2].
+                rewrite singleton_eq in a_in2; subst a.
+                contradiction.
     }
     apply (well_order_sets_max [A|A_wo]).
     split.
