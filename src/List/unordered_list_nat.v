e2c71bfc546c8f24f59a21e273a5f64406ffa510
David Cohoe <sudgylacmoe@gmail.com>
2023-04-08 09:03:54 -0700

    Defined ulist_count
    While I don't need it right now, I like having everything possible from
    list being defined in ulist.

--- src/List/unordered_list_nat.v
+++ src/List/unordered_list_nat.v
@@ -31,6 +31,9 @@ Proof.
 Qed.
 Definition ulist_size {U} := unary_op (E := ulist_equiv U) (ulist_size_wd U).
 
+Definition ulist_count {U} (l : ulist U) (a : U)
+    := unary_op (E := ulist_equiv U) (λ l1 l2 eq, eq a) l.
+
 Theorem ulist_size_end {U} : ulist_size (@ulist_end U) = 0.
 Proof.
     unfold ulist_size, ulist_end; equiv_simpl.
@@ -69,4 +72,103 @@ Proof.
     equiv_get_value l.
     unfold ulist_size, ulist_image; equiv_simpl.
     apply list_image_size.
+Qed.
+
+Theorem ulist_count_end {U} : ∀ (x : U), ulist_count ⟦⟧ x = 0.
+Proof.
+    intros x.
+    unfold ulist_count, ulist_end; equiv_simpl.
+    apply list_count_end.
+Qed.
+
+Theorem ulist_count_add {U} : ∀ (x y : U) a,
+    ulist_count (x ː a) y = (If x = y then 1 else 0) + ulist_count a y.
+Proof.
+    intros x y a.
+    equiv_get_value a.
+    unfold ulist_count, ulist_add; equiv_simpl.
+    apply list_count_add.
+Qed.
+
+Theorem ulist_count_add_eq {U} : ∀ (x : U) a,
+    ulist_count (x ː a) x = nat_suc (ulist_count a x).
+Proof.
+    intros x a.
+    rewrite ulist_count_add.
+    rewrite (if_true (Logic.eq_refl _)).
+    reflexivity.
+Qed.
+
+Theorem ulist_count_add_neq {U} : ∀ {x y : U} {a}, x ≠ y →
+    ulist_count (x ː a) y = ulist_count a y.
+Proof.
+    intros x y a neq.
+    rewrite ulist_count_add.
+    rewrite (if_false neq).
+    rewrite plus_lid.
+    reflexivity.
+Qed.
+
+Theorem ulist_count_single {U} : ∀ (x y : U),
+    ulist_count ⟦x⟧ y = If x = y then 1 else 0.
+Proof.
+    intros x y.
+    rewrite ulist_count_add, ulist_count_end.
+    rewrite plus_rid.
+    reflexivity.
+Qed.
+
+Theorem ulist_count_single_eq {U} : ∀ x : U, ulist_count ⟦x⟧ x = 1.
+Proof.
+    intros x.
+    rewrite ulist_count_add_eq, ulist_count_end.
+    reflexivity.
+Qed.
+
+Theorem ulist_count_single_neq {U} : ∀ x y : U, x ≠ y → ulist_count ⟦x⟧ y = 0.
+Proof.
+    intros x y neq.
+    rewrite (ulist_count_add_neq neq).
+    apply ulist_count_end.
+Qed.
+
+Theorem ulist_count_conc {U} : ∀ l1 l2 (a : U),
+    ulist_count (l1 + l2) a = ulist_count l1 a + ulist_count l2 a.
+Proof.
+    intros l1 l2 a.
+    equiv_get_value l1 l2.
+    unfold ulist_count, plus at 1; equiv_simpl.
+    apply list_count_conc.
+Qed.
+
+Theorem ulist_count_in {U} : ∀ l (a : U), in_ulist l a ↔ 0 ≠ ulist_count l a.
+Proof.
+    intros l a.
+    equiv_get_value l.
+    unfold in_ulist, ulist_count; equiv_simpl.
+    apply list_count_in.
+Qed.
+
+Theorem ulist_count_nin {U} : ∀ l (a : U), ¬in_ulist l a ↔ 0 = ulist_count l a.
+Proof.
+    intros l a.
+    rewrite ulist_count_in.
+    apply not_not.
+Qed.
+
+Theorem ulist_count_unique {U} : ∀ l (a : U), ulist_unique l → ulist_count l a ≤ 1.
+Proof.
+    intros l x.
+    equiv_get_value l.
+    unfold ulist_unique, ulist_count; equiv_simpl.
+    apply list_count_unique.
+Qed.
+
+Theorem ulist_count_in_unique {U} : ∀ l (a : U), ulist_unique l → in_ulist l a →
+    ulist_count l a = 1.
+Proof.
+    intros l a.
+    equiv_get_value l.
+    unfold ulist_unique, in_ulist, ulist_count; equiv_simpl.
+    apply list_count_in_unique.
 Qed.
