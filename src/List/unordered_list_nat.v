0ce6eab105e575cbbf3c1a84138564606649dac6
David Cohoe <sudgylacmoe@gmail.com>
2023-04-08 18:49:23 -0700

    Cleaned up unordered_list_nat.v a bit

--- src/List/unordered_list_nat.v
+++ src/List/unordered_list_nat.v
@@ -10,17 +10,11 @@ Require Import equivalence.
 Lemma ulist_size_wd U : ∀ l1 l2 : list U, list_permutation l1 l2 →
     list_size l1 = list_size l2.
 Proof.
-    intros l1 l2 eq.
-    revert l2 eq.
-    induction l1 as [|a l1]; intros.
+    induction l1 as [|a l1]; intros l2 eq.
     -   apply list_perm_nil_eq in eq.
         destruct eq.
         reflexivity.
-    -   assert (in_list (a ꞉ l1) a) as a_in by (left; reflexivity).
-        apply (list_perm_in eq) in a_in.
-        apply in_list_split in a_in as [l3 [l4 l2_eq]]; subst l2.
-        apply (trans2 (list_perm_split l3 l4 a)) in eq.
-        apply list_perm_add_eq in eq.
+    -   apply list_perm_split_eq in eq as [l3 [l4 [l_eq eq]]]; subst l2.
         specialize (IHl1 _ eq).
         rewrite list_size_comm.
         rewrite list_conc_add.
@@ -37,7 +31,7 @@ Definition ulist_count {U} (l : ulist U) (a : U)
 Theorem ulist_size_end {U} : ulist_size (@ulist_end U) = 0.
 Proof.
     unfold ulist_size, ulist_end; equiv_simpl.
-    reflexivity.
+    apply list_size_end.
 Qed.
 
 Theorem ulist_size_add {U} : ∀ (a : U) l,
@@ -46,7 +40,7 @@ Proof.
     intros a l.
     equiv_get_value l.
     unfold ulist_size, ulist_add; equiv_simpl.
-    reflexivity.
+    apply list_size_add.
 Qed.
 
 Theorem ulist_size_single {U} : ∀ x : U, ulist_size ⟦x⟧ = 1.
@@ -61,7 +55,7 @@ Theorem ulist_size_conc {U} : ∀ l1 l2 : ulist U,
 Proof.
     intros l1 l2.
     equiv_get_value l1 l2.
-    unfold ulist_size, plus; equiv_simpl.
+    unfold ulist_size, plus at 1; equiv_simpl.
     apply list_size_conc.
 Qed.
 
