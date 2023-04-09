8486353a8c48053d91f626d51c27094216b1b105
David Cohoe <sudgylacmoe@gmail.com>
2023-04-08 19:06:17 -0700

    Cleaned up unordered_list_fold.v a bit

--- src/List/unordered_list_fold.v
+++ src/List/unordered_list_fold.v
@@ -8,22 +8,9 @@ Require Export unordered_list_nat.
 
 Require Import equivalence.
 
-(* begin hide *)
-Unset Keyed Unification.
+Section Fold.
 
-Section Sum.
-
-Context {U} `{
-    UP : Plus U,
-    UZ : Zero U,
-    UN : Neg U,
-    @PlusAssoc U UP,
-    @PlusComm U UP,
-    @PlusLid U UP UZ,
-    @PlusLinv U UP UZ UN
-}.
-
-(* end hide *)
+Context {U} `{CRingClass U}.
 
 Theorem ulist_sum_wd : ∀ l1 l2, list_permutation l1 l2 →
     list_sum l1 = list_sum l2.
@@ -32,23 +19,16 @@ Proof.
     -   apply list_perm_nil_eq in eq.
         subst l2.
         reflexivity.
-    -   assert (in_list (a ꞉ l1) a) as a_in by (left; reflexivity).
-        apply (list_perm_in eq) in a_in.
-        apply in_list_split in a_in as [l3 [l4 l2_eq]]; subst l2.
+    -   apply list_perm_split_eq in eq as [l3 [l4 [l_eq eq]]]; subst l2.
         rewrite list_sum_conc.
         do 2 rewrite list_sum_add.
-        pose proof (list_perm_split l3 l4 a) as eq2.
-        pose proof (trans eq eq2) as eq3.
-        apply list_perm_add_eq in eq3.
-        rewrite (IHl1 _ eq3).
+        rewrite (IHl1 _ eq).
         rewrite list_sum_conc.
-        do 2 rewrite plus_assoc.
-        apply rplus.
-        apply plus_comm.
+        apply plus_3.
 Qed.
 Definition ulist_sum := unary_op (E := ulist_equiv U) ulist_sum_wd.
 
-Theorem ulist_sum_end : ulist_sum ulist_end = 0.
+Theorem ulist_sum_end : ulist_sum ⟦⟧ = 0.
 Proof.
     unfold ulist_sum, ulist_end; equiv_simpl.
     reflexivity.
@@ -59,7 +39,7 @@ Proof.
     intros a l.
     equiv_get_value l.
     unfold ulist_sum, ulist_add; equiv_simpl.
-    reflexivity.
+    apply list_sum_add.
 Qed.
 
 Theorem list_sum_single : ∀ x, ulist_sum ⟦x⟧ = x.
@@ -73,7 +53,7 @@ Theorem ulist_sum_conc : ∀ a b, ulist_sum (a + b) = ulist_sum a + ulist_sum b.
 Proof.
     intros a b.
     equiv_get_value a b.
-    unfold ulist_sum, plus; equiv_simpl.
+    unfold ulist_sum, plus at 1; equiv_simpl.
     apply list_sum_conc.
 Qed.
 
@@ -93,23 +73,7 @@ Proof.
     rewrite ulist_sum_neg.
     reflexivity.
 Qed.
-(* begin hide *)
 
-End Sum.
-(* end hide *)
-
-(* begin hide *)
-Section Product.
-
-Context {U} `{
-    UM : Mult U,
-    UO : One U,
-    @MultAssoc U UM,
-    @MultComm U UM,
-    @MultLid U UM UO
-}.
-
-(* end hide *)
 Theorem list_prod_perm : ∀ l1 l2, list_permutation l1 l2 →
     list_prod l1 = list_prod l2.
 Proof.
@@ -117,15 +81,10 @@ Proof.
     -   apply list_perm_nil_eq in eq.
         subst l2.
         reflexivity.
-    -   assert (in_list (a ꞉ l1) a) as a_in by (left; reflexivity).
-        apply (list_perm_in eq) in a_in.
-        apply in_list_split in a_in as [l3 [l4 l2_eq]]; subst l2.
+    -   apply list_perm_split_eq in eq as [l3 [l4 [l_eq eq]]]; subst l2.
         rewrite list_prod_conc.
         do 2 rewrite list_prod_add.
-        pose proof (list_perm_split l3 l4 a) as eq2.
-        pose proof (trans eq eq2) as eq3.
-        apply list_perm_add_eq in eq3.
-        rewrite (IHl1 _ eq3).
+        rewrite (IHl1 _ eq).
         rewrite list_prod_conc.
         do 2 rewrite mult_assoc.
         apply rmult.
@@ -136,7 +95,7 @@ Definition ulist_prod := unary_op (E := ulist_equiv U) list_prod_perm.
 Theorem ulist_prod_end : ulist_prod ulist_end = 1.
 Proof.
     unfold ulist_prod, ulist_end; equiv_simpl.
-    reflexivity.
+    apply list_prod_end.
 Qed.
 
 Theorem ulist_prod_add : ∀ a l, ulist_prod (a ː l) = a * ulist_prod l.
@@ -144,7 +103,7 @@ Proof.
     intros a l.
     equiv_get_value l.
     unfold ulist_prod, ulist_add; equiv_simpl.
-    reflexivity.
+    apply list_prod_add.
 Qed.
 
 Theorem ulist_prod_single : ∀ x, ulist_prod ⟦x⟧ = x.
@@ -162,6 +121,5 @@ Proof.
     unfold ulist_prod, plus; equiv_simpl.
     apply list_prod_conc.
 Qed.
-(* begin hide *)
-End Product.
-(* end hide *)
+
+End Fold.
