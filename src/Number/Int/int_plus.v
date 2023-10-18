093e1f672c1d31196981f1505a3205f482d80a19
David Cohoe <sudgylacmoe@gmail.com>
2023-10-18 13:21:03 -0700

    Cleaned up int_plus

--- src/Number/Int/int_plus.v
+++ src/Number/Int/int_plus.v
@@ -8,62 +8,52 @@ Require Export int_base.
 
 Notation "a ⊕ b" := (fst a + fst b, snd a + snd b) : int_scope.
 
-(* begin hide *)
-Section IntPlus.
-
-Local Open Scope int_scope.
+Open Scope int_scope.
 
 Lemma int_plus_wd : ∀ a b c d, a ~ b → c ~ d → a ⊕ c ~ b ⊕ d.
 Proof.
     intros [a1 a2] [b1 b2] [c1 c2] [d1 d2] ab cd.
     cbn in *.
-    pose proof (lrplus ab cd) as eq; clear ab cd.
-    do 2 rewrite <- plus_assoc in eq.
-    rewrite (plus_assoc b2) in eq; rewrite (plus_assoc a2) in eq.
-    rewrite (plus_comm b2) in eq; rewrite (plus_comm a2) in eq.
-    repeat rewrite plus_assoc in eq; repeat rewrite plus_assoc.
-    exact eq.
+    rewrite plus_4.
+    rewrite ab, cd.
+    apply plus_4.
 Qed.
-
-End IntPlus.
 
 Global Instance int_plus : Plus int := {
     plus := binary_op (binary_self_wd int_plus_wd)
 }.
 
-Lemma int_plus_comm : ∀ a b, a + b = b + a.
+Global Instance int_plus_comm_class : PlusComm int.
 Proof.
+    split.
     intros a b.
     equiv_get_value a b.
     destruct a as [a1 a2], b as [b1 b2].
     unfold plus; equiv_simpl.
     rewrite (plus_comm a1).
-    rewrite (plus_comm a2).
+    rewrite (plus_comm b2).
     reflexivity.
 Qed.
-Global Instance int_plus_comm_class : PlusComm int := {
-    plus_comm := int_plus_comm
-}.
 
-Lemma int_plus_assoc : ∀ a b c, a + (b + c) = (a + b) + c.
+Global Instance int_plus_assoc_class : PlusAssoc int.
 Proof.
+    split.
     intros a b c.
     equiv_get_value a b c.
     destruct a as [a1 a2], b as [b1 b2], c as [c1 c2].
     unfold plus; equiv_simpl.
-    repeat rewrite plus_assoc.
+    rewrite (plus_assoc a1), (plus_assoc a2).
     reflexivity.
 Qed.
-Global Instance int_plus_assoc_class : PlusAssoc int := {
-    plus_assoc := int_plus_assoc
-}.
 
 Global Instance int_zero : Zero int := {
-    zero := to_equiv int_equiv (zero, zero);
+    zero := to_equiv int_equiv (0, 0);
 }.
 
-Lemma int_plus_lid : ∀ a, zero + a = a.
+
+Global Instance int_plus_lid_class : PlusLid int.
 Proof.
+    split.
     intros a.
     equiv_get_value a.
     unfold zero, plus; equiv_simpl.
@@ -71,16 +61,7 @@ Proof.
     reflexivity.
 Qed.
 
-Global Instance int_plus_lid_class : PlusLid int := {
-    plus_lid := int_plus_lid;
-}.
-(* end hide *)
-Notation "⊖ a" := (snd a, fst a) (at level 35, right associativity) : int_scope.
-
-(* begin hide *)
-Section IntNeg.
-
-Local Open Scope int_scope.
+Notation "⊖ a" := (snd a, fst a) : int_scope.
 
 Lemma int_neg_wd : ∀ a b, a ~ b → ⊖a ~ ⊖b.
 Proof.
@@ -91,14 +72,13 @@ Proof.
     exact eq.
 Qed.
 
-End IntNeg.
-
 Global Instance int_neg : Neg int := {
     neg := unary_op (unary_self_wd int_neg_wd);
 }.
 
-Lemma int_plus_linv : ∀ a, -a + a = zero.
+Global Instance int_plus_linv_class : PlusLinv int.
 Proof.
+    split.
     intros a.
     equiv_get_value a.
     unfold zero, plus, neg; equiv_simpl.
@@ -106,7 +86,4 @@ Proof.
     apply plus_comm.
 Qed.
 
-Global Instance int_plus_linv_class : PlusLinv int := {
-    plus_linv := int_plus_linv;
-}.
-(* end hide *)
+Close Scope int_scope.
