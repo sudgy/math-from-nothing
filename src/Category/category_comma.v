050d10c75df43320cad3266718f866ef737b5b2f
David Cohoe <sudgylacmoe@gmail.com>
2023-10-24 02:13:26 -0700

    Added definition of free functors from comma cats
    I know that this construction from comma categories doesn't always
    produce a free functor, but for my purposes it does.

--- src/Category/category_comma.v
+++ src/Category/category_comma.v
@@ -2,6 +2,7 @@ Require Import init.
 
 Require Export category_def.
 Require Export basic_categories.
+Require Import category_initterm.
 Require Export set.
 
 Set Universe Polymorphism.
@@ -83,5 +84,86 @@ Definition make_slice {C : Category} (a : C) (b : C) (f : morphism b a)
     := make_comma 𝟙 (obj_to_functor a) b Single f.
 Definition make_coslice {C : Category} (a : C) (b : C) (f : morphism a b)
     := make_comma (obj_to_functor a) 𝟙 Single b f.
+
+Section FreeFunctor.
+
+Context {B C : Category} {T : Functor B C}
+    {f : C → B} {g : ∀ a : C, morphism a (T (f a))}.
+
+Hypothesis i : ∀ a : C, initial (make_comma_l1 a T (f a) (g a)).
+
+Program Definition free_functor : Functor C B := {|
+    functor_f := f;
+    functor_morphism a b h
+        := snd [ex_singleton (i a (make_comma_l1 a T (f b) ((g b) ∘ h)))|]
+|}.
+Next Obligation.
+    rename A into a, B0 into b, C0 into c.
+    rename f0 into h1, g0 into h2.
+    pose (mi x y h := i x (make_comma_l1 x T (f y) (g y ∘ h))).
+    fold (mi a c (h1 ∘ h2)).
+    fold (mi b c h1).
+    fold (mi a b h2).
+    pose proof (singleton_unique2 (ex_singleton (mi a c (h1 ∘ h2)))) as eq.
+    cbn in eq.
+    unfold comma_set in eq; cbn in eq.
+    pose (h := snd [ex_singleton(mi b c h1)|] ∘ snd [ex_singleton(mi a b h2)|]).
+    assert (⌈T⌉ h ∘ g a = g c ∘ (h1 ∘ h2) ∘ 𝟙) as h_in.
+    {
+        unfold h.
+        clear eq h.
+        destruct (ex_singleton _) as [[s1 fh1] fh1_in].
+        destruct (ex_singleton _) as [[s2 fh2] fh2_in].
+        unfold comma_set in fh1_in, fh2_in.
+        cbn in *.
+        clear s1 s2.
+        rewrite functor_compose.
+        rewrite <- cat_assoc.
+        rewrite fh2_in.
+        do 2 rewrite cat_rid.
+        rewrite cat_assoc.
+        rewrite fh1_in.
+        rewrite cat_rid.
+        symmetry; apply cat_assoc.
+    }
+    rewrite (eq [(Single, h)|h_in]).
+    cbn.
+    unfold h.
+    reflexivity.
+Qed.
+Next Obligation.
+    rename A into a.
+    pose (mi x h := i x (make_comma_l1 x T (f x) (g x ∘ h))).
+    fold (mi a 𝟙).
+    pose proof (singleton_unique2 (ex_singleton (mi a 𝟙))) as eq.
+    cbn in eq.
+    unfold comma_set in eq; cbn in eq.
+    assert (⌈T⌉ 𝟙 ∘ g a = g a ∘ 𝟙 ∘ 𝟙) as i_in.
+    {
+        rewrite functor_id.
+        do 2 rewrite cat_rid.
+        apply cat_lid.
+    }
+    rewrite (eq [(Single, 𝟙)|i_in]).
+    cbn.
+    reflexivity.
+Qed.
+
+Local Notation "'F'" := free_functor.
+
+Theorem free_commute : ∀ {a b : C} (h : morphism a b),
+    (⌈T ∘ F⌉ h) ∘ g a = g b ∘ h.
+Proof.
+    intros a b h.
+    cbn.
+    destruct (ex_singleton _) as [[s fh] fh_in]; cbn.
+    unfold comma_set in fh_in; cbn in fh_in.
+    rewrite cat_rid in fh_in.
+    exact fh_in.
+Qed.
+
+Arguments free_functor : simpl never.
+
+End FreeFunctor.
 
 Unset Universe Polymorphism.
