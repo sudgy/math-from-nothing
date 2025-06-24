9efba9c68349119069cb708e02ea7e2e774ff26e
David Cohoe <sudgylacmoe@gmail.com>
2025-06-24 11:00:35 -0700

    Proved some extra facts about ord_to_card

--- src/Set/Cardinal/New/card2_mult.v
+++ src/Set/Cardinal/New/card2_mult.v
@@ -1,6 +1,7 @@
 Require Import init.
 
 Require Export card2_plus.
+Require Import ord.
 
 Open Scope card_scope.
 
@@ -140,6 +141,107 @@ Proof.
     apply prod_split in eq as [eq1 eq2].
     apply f_inj in eq2.
     subst; reflexivity.
+Qed.
+
+Global Instance ord_to_card_one : HomomorphismOne ord_to_card.
+Proof.
+    split.
+    unfold one, ord_to_card; cbn.
+    rewrite unary_op_eq.
+    reflexivity.
+Qed.
+
+Global Instance ord_to_card_mult : HomomorphismMult ord_to_card.
+Proof.
+    split.
+    intros A B.
+    equiv_get_value A B.
+    unfold ord_to_card at 2 3.
+    unfold mult at 2; equiv_simpl.
+    symmetry; apply ord_to_card_eq2.
+    pose (α := to_ord A).
+    pose (β := to_ord B).
+    fold α β.
+    assert (∀ a b, α * ord_type_init_ord_base B b
+                     + ord_type_init_ord_base A a < α * β) as f_lt.
+    {
+        intros a b.
+        pose proof (ord_type_init_ord_in a) as a_lt.
+        pose proof (ord_type_init_ord_in b) as b_lt.
+        unfold initial_segment in a_lt, b_lt.
+        fold α in a_lt.
+        fold β in b_lt.
+        apply (lt_lplus (α * ord_type_init_ord_base B b)) in a_lt.
+        apply (lt_le_trans a_lt).
+        rewrite <- ord_ldist_one.
+        apply le_lmult.
+        rewrite ord_le_suc_lt.
+        exact b_lt.
+    }
+    exists (λ x, [α * ord_type_init_ord_base B (snd x)
+                    + ord_type_init_ord_base A (fst x) | f_lt (fst x) (snd x)]).
+    pose proof (ord_type_init_ord_base_inj A).
+    pose proof (ord_type_init_ord_base_inj B).
+    pose proof (ord_type_init_ord_bij A).
+    pose proof (ord_type_init_ord_bij B).
+    split; split.
+    -   intros [a1 b1] [a2 b2]; cbn; intros eq.
+        assert (0 ≠ α) as α_nz.
+        {
+            intros α_z.
+            pose proof (ord_type_init_ord_in a1) as ltq.
+            unfold initial_segment in ltq.
+            fold α in ltq.
+            rewrite <- α_z in ltq.
+            contradiction (not_neg ltq).
+        }
+        rewrite set_type_eq2 in eq.
+        pose proof (ord_type_init_ord_in a1) as ltq1.
+        unfold initial_segment in ltq1; fold α in ltq1.
+        apply (lt_lplus (α * ord_type_init_ord_base B b1)) in ltq1.
+        rewrite eq in ltq1.
+        pose proof (ord_type_init_ord_in a2) as ltq2.
+        unfold initial_segment in ltq2; fold α in ltq2.
+        apply (lt_lplus (α * ord_type_init_ord_base B b2)) in ltq2.
+        rewrite <- eq in ltq2.
+        apply (le_lt_trans (ord_le_self_rplus _ _)) in ltq1.
+        apply (le_lt_trans (ord_le_self_rplus _ _)) in ltq2.
+        rewrite <- ord_ldist_one in ltq1, ltq2.
+        apply (lt_mult_lcancel α α_nz) in ltq1, ltq2.
+        rewrite ord_lt_suc_le in ltq1, ltq2.
+        pose proof (antisym ltq1 ltq2) as eq'.
+        apply inj in eq'.
+        subst b2.
+        apply plus_lcancel in eq.
+        apply inj in eq.
+        subst a2.
+        reflexivity.
+    -   intros [γ γ_lt].
+        unfold initial_segment in γ_lt.
+        assert (0 ≠ α) as α_nz.
+        {
+            intros α_z.
+            rewrite <- α_z in γ_lt.
+            rewrite mult_lanni in γ_lt.
+            contradiction (not_neg γ_lt).
+        }
+        pose proof (ord_div γ α α_nz) as [ε [δ [γ_eq δ_lt]]].
+        assert (ε < β) as ε_lt.
+        {
+            apply (lt_mult_lcancel α α_nz).
+            apply (le_lt_trans2 γ_lt).
+            rewrite γ_eq.
+            apply ord_le_self_rplus.
+        }
+        pose proof (sur (ord_type_init_ord A) [δ|δ_lt]) as [a a_eq].
+        pose proof (sur (ord_type_init_ord B) [ε|ε_lt]) as [b b_eq].
+        exists (a, b).
+        rewrite set_type_eq2; cbn.
+        unfold ord_type_init_ord in a_eq, b_eq.
+        rewrite set_type_eq2 in a_eq.
+        rewrite set_type_eq2 in b_eq.
+        rewrite a_eq, b_eq.
+        symmetry; exact γ_eq.
 Qed.
 
 Close Scope card_scope.
--- src/Set/Cardinal/New/card2_plus.v
+++ src/Set/Cardinal/New/card2_plus.v
@@ -2,6 +2,7 @@ Require Import init.
 
 Require Export card2_order.
 Require Export set.
+Require Import ord.
 
 Open Scope card_scope.
 
@@ -146,6 +147,94 @@ Proof.
     classic_contradiction contr.
     apply card_false_0 in contr.
     contradiction.
+Qed.
+
+Global Instance ord_to_card_zero : HomomorphismZero ord_to_card.
+Proof.
+    split.
+    unfold ord_to_card, zero; cbn.
+    rewrite unary_op_eq; cbn.
+    reflexivity.
+Qed.
+
+Global Instance ord_to_card_plus : HomomorphismPlus ord_to_card.
+Proof.
+    split.
+    intros A B.
+    equiv_get_value A B.
+    unfold ord_to_card at 2 3.
+    unfold plus at 2; equiv_simpl.
+    symmetry; apply ord_to_card_eq2.
+    pose (α := to_ord A).
+    pose (β := to_ord B).
+    fold α β.
+    assert (∀ a : A, ord_type_init_ord_base A a < α + β)
+        as f_lt1.
+    {
+        intros a.
+        apply (lt_le_trans2 (ord_le_self_rplus _ _)).
+        unfold α, ord_type_init_ord_base.
+        rewrite ord_lt_simpl.
+        exists a.
+        apply refl.
+    }
+    assert (∀ b : B, α + ord_type_init_ord_base B b < α + β)
+        as f_lt2.
+    {
+        intros b.
+        apply lt_lplus.
+        unfold β, ord_type_init_ord_base.
+        rewrite ord_lt_simpl.
+        exists b.
+        apply refl.
+    }
+    exists (λ x, match x with
+        | inl a => [ord_type_init_ord_base A a |f_lt1 a]
+        | inr b => [α + ord_type_init_ord_base B b |f_lt2 b]
+        end).
+    pose proof (ord_type_init_ord_base_inj A).
+    pose proof (ord_type_init_ord_base_inj B).
+    pose proof (ord_type_init_ord_bij A).
+    pose proof (ord_type_init_ord_bij B).
+    split; split.
+    -   intros [a1|b1] [a2|b2] eq; rewrite set_type_eq2 in eq.
+        +   apply inj in eq.
+            subst; reflexivity.
+        +   pose proof (ord_type_init_ord_in a1) as ltq.
+            unfold initial_segment in ltq.
+            fold α in ltq.
+            rewrite eq in ltq.
+            apply (le_lt_trans (ord_le_self_rplus _ _)) in ltq.
+            contradiction (irrefl _ ltq).
+        +   pose proof (ord_type_init_ord_in a2) as ltq.
+            unfold initial_segment in ltq.
+            fold α in ltq.
+            rewrite <- eq in ltq.
+            apply (le_lt_trans (ord_le_self_rplus _ _)) in ltq.
+            contradiction (irrefl _ ltq).
+        +   apply plus_lcancel in eq.
+            apply inj in eq.
+            subst; reflexivity.
+    -   intros [γ γ_lt].
+        unfold initial_segment in γ_lt.
+        classic_case (γ < α) as [γ_lt2|γ_ge].
+        +   pose proof (sur (ord_type_init_ord A) [γ|γ_lt2]) as [a a_eq].
+            exists (inl a).
+            apply set_type_eq2.
+            unfold ord_type_init_ord in a_eq.
+            rewrite set_type_eq2 in a_eq.
+            exact a_eq.
+        +   rewrite nlt_le in γ_ge.
+            apply ord_le_ex in γ_ge as [δ δ_eq]; subst γ.
+            pose proof γ_lt as δ_lt.
+            apply lt_plus_lcancel in δ_lt.
+            pose proof (sur (ord_type_init_ord B) [δ|δ_lt]) as [b b_eq].
+            exists (inr b).
+            apply set_type_eq2.
+            apply lplus.
+            unfold ord_type_init_ord in b_eq.
+            rewrite set_type_eq2 in b_eq.
+            exact b_eq.
 Qed.
 
 Close Scope card_scope.
